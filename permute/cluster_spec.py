48210c4360bb690d63515cdf3af668c24bd9bc04
jed irvine <irvine@eecs.oregonstate.edu>
2013-11-15 13:32:05 -0800

    tests added for cluster_spec loading

--- permute/cluster_spec.py
+++ permute/cluster_spec.py
@@ -1,11 +1,13 @@
 import os, time
 
+verbose = False
+
 class ClusterSpec(object):
     '''
     Wraps the cluster specification file *.cspec
     '''
     
-    def __init(self, path):
+    def __init__(self, path):
         '''
         Constructor
         '''
@@ -21,24 +23,39 @@ class ClusterSpec(object):
                 exit()
             f.close()
             
-            self.permutes = load_permutes(self.path)
-            self.concise_print_map = load_concise_print_map(self.path)
-            self.key_val_map = load_key_val_map(self.path)
-            self.qsub_commands = load_qsub_commands(self.path,self.key_val_map)
-            self.commands = load_commands(self.path,self.key_val_map)
+            self.permutations = self.load_permutations(self.path)
+            self.concise_print_map = self.load_concise_print_map(self.path)
+            self.key_val_map = self.load_key_val_map(self.path)
+            self.qsub_commands = self.load_qsub_commands(self.path,self.key_val_map)
+            self.commands = self.load_commands(self.path,self.key_val_map)
         except IOError:
             print "An error occured trying to open cspec file {0}".format(path)
             exit()
 
+      
+    def if_verbose(self, message):
+        global verbose
+        if (verbose):
+            print message
 
-    def load_permutes(self, path):
+    def resolve_value(self, keyValMap, given_val):
+        result = given_val
+        for key, val in keyValMap.iteritems():
+            match_string = "<{0}>".format(key)
+            result = result.replace(match_string, val)
+        self.if_verbose("  value resolved to : {0}".format(result))
+        return result
+    
+    def load_permutations(self, path):
         f = open(path, 'r')
         permuters = {}
         lines = f.readlines()
         for line in lines:
             line = line.rstrip()
             if (line.startswith("permute:")):
-                if_verbose("  processing permute line - {0}".format(line))
+                #foo = "  processing permute line - {0}".format(line)
+                #self.if_verbose(foo)
+                self.if_verbose("  processing permute line - {0}".format(line))
                 foo, permuteKey, permute_list_string = line.split(':')
                 if (permute_list_string.find(" ") != -1):
                     permute_start, permute_end = permute_list_string.split(" ")
@@ -63,9 +80,9 @@ class ClusterSpec(object):
             if (line.startswith("#")):
                 pass
             elif (line.find("=") != -1):
-                if_verbose("  processing keyVal line - {0}".format(line))
+                self.if_verbose("  processing keyVal line - {0}".format(line))
                 key, val = line.split("=")
-                val = resolve_value(key_val_map, val)
+                val = self.resolve_value(key_val_map, val)
                 key_val_map[key] = val
             else:
                 pass
@@ -80,7 +97,7 @@ class ClusterSpec(object):
         for line in lines:
             line = line.rstrip()
             if (line.startswith("qsub_command:")):
-                if_verbose("  processing qsub_command line - {0}".format(line))
+                self.if_verbose("  processing qsub_command line - {0}".format(line))
                 x, this_command = line.split(":")
                 qsub_commands.append(this_command)
                 #resolve_command(this_command, key_val_map, qsub_commands)
@@ -96,7 +113,7 @@ class ClusterSpec(object):
         for line in lines:
             line = line.rstrip()
             if (line.startswith("command:")):
-                if_verbose("  processing command line - {0}".format(line))
+                self.if_verbose("  processing command line - {0}".format(line))
                 x, this_command = line.split(":")
                 commands.append(this_command)
                 #resolve_command(this_command, key_val_map, commands)
@@ -113,7 +130,7 @@ class ClusterSpec(object):
         for line in lines:
             line = line.rstrip()
             if (line.startswith("concise_print:")):
-                if_verbose("  processing concise_print line - {0}".format(line))
+                self.if_verbose("  processing concise_print line - {0}".format(line))
                 command, conciseKeyVal = line.split(":")
                 key, val = conciseKeyVal.split(",")
                 concisePrintMap[key] = val
--- test/test_cluster_spec.py
+++ test/test_cluster_spec.py
@@ -1,12 +1,49 @@
 import unittest
+from permute import cluster_spec
 
 class TestClusterSpec(unittest.TestCase):
 
+    def setUp(self):
+        path = "./test.cspec"
+        self.cspec = cluster_spec.ClusterSpec(path)
+        
     def test_load_permutes(self):
-        print"hello"
-        self.assertTrue(True)
+        permutations_as_string = "{0}".format(self.cspec.permutations)
+        self.assertTrue(permutations_as_string == "{'number': ['1', '2', '3'], 'letter': ['AAA', 'BBB']}")
+        
+    def test_concise_print_map(self):
+        concise_print_map_as_string = "{0}".format(self.cspec.concise_print_map)
+        self.assertTrue(concise_print_map_as_string == "{'letter': 'l', 'month': 'm'}")
+           
+    def test_key_val_map(self):
+        kvm = self.cspec.key_val_map
+        self.assertTrue(kvm['config[AAA]']=='aaa')
+        self.assertTrue(kvm['config[BBB]']=='bbb')
+        self.assertTrue(kvm['pretty[1]']=='one')
+        self.assertTrue(kvm['pretty[2]']=='two')
+        self.assertTrue(kvm['pretty[3]']=='three')
+        self.assertTrue(kvm['permutation_set_name']=='unittest')
+        self.assertTrue(kvm['root']=='/nfs/foo/bar')
+        self.assertTrue(kvm['x_dir']=='/nfs/foo/bar/(letter)/<config[(letter)]>/(number)')
+        self.assertTrue(kvm['algs_dir']=='/nfs/algs')
+        self.assertTrue(kvm['tools_dir']=='/nfs/algs/tools')
+        self.assertTrue(kvm['results_root']=='/nfs/foo/results/unittest')
+        self.assertTrue(kvm['results_dir']=='/nfs/foo/results/unittest/(number)/(letter)')
+        self.assertTrue(kvm['outfile_root']=='<pretty[(number)]>__TEST')
+        self.assertTrue(kvm['dir_script']=='./scripts_unittest')
+        self.assertTrue(kvm['one_up_basis']=='100')
+            
+    def test_commands(self):
+        commands_as_string = "{0}".format(self.cspec.commands)
+        self.assertTrue(commands_as_string == "['ls -la /nfs/missing > <x_dir>/<pretty[(number)]>.txt']")
+           
+    def test__qsub_commands(self):
+        qsub_commands_as_string = "{0}".format(self.cspec.qsub_commands)
+        self.assertTrue(qsub_commands_as_string == "['-q eecs,eecs1,eecs,share', '-M someone@gmail.com', '-m beas']")
+           
 
-
+           
 if __name__ == '__main__':
     unittest.main()
     
+   
