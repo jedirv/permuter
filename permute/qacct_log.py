4c0ba5cb75151d463f7333e265e9ce1336bca6a8
Jed Irvine <jedirv@gmail.com>
2014-01-07 22:42:27 -0800

    more print statements

--- permute/qacct_log.py
+++ permute/qacct_log.py
@@ -91,9 +91,10 @@ class QacctLog(PermutationDriverFile):
                 self.qsub_time = parts[1]
             elif (parts[0] == 'start_time'):
                 if (parts[1] == '-/-'):
+                    print 'start time is -/-'
                     self.start_time = parts[1]
                 else:
-                    #print 'start time parts : {0}'.format(line)
+                    print 'start time parts : {0}'.format(line)
                     self.start_time = "{0} {1} {2} {3} {4}".format(parts[1], parts[2], parts[3], parts[4], parts[5])
             elif (parts[0] == 'end_time'):
                 self.end_time = "{0} {1} {2} {3} {4}".format(parts[1], parts[2], parts[3], parts[4], parts[5])
