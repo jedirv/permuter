370e98c51b6093167cc69427ab22fcb24c50ea9a
Jed Irvine <jedirv@gmail.com>
2014-03-04 21:11:58 -0800

    adding class stat_of_runs

---
+++ permute/state_of_runs.py
@@ -0,0 +1,54 @@
+'''
+Created on Feb 23, 2014
+
+@author: irvine
+'''
+
+class StateOfRuns(object):
+    '''
+    classdocs
+    '''
+    def __init__(self):
+        '''
+        Constructor
+        '''
+        self.scripts_exist = False
+        self.all_scripts_exist = False
+        self.runs_in_progress = False
+        self.runs_completed = False
+        self.pooled_results_exist = False
+        self.pooled_timings_exist = False
+        self.ranked_results_exist = False
+        
+        
+    def assess(self, cur_cluster_system, cluster_runs_info):    
+        self.cur_cluster_system = cur_cluster_system
+        self.cluster_runs_info = cluster_runs_info
+        self.scripts_exist = check_for_any_scripts(cur_cluster_system, cluster_runs_info)
+        self.all_scripts_exist = check_for_all_scripts(cur_cluster_system, cluster_runs_info)
+        self.runs_in_progress = check_for_runs_in_progress(cur_cluster_system, cluster_runs_info)
+        self.runs_completed = check_for_runs_complete(cur_cluster_system,cluster_runs_info)
+        self.pooled_results_exist = check_for_pooled_results(cur_cluster_system,cluster_runs_info)
+        self.pooled_timings_exist = check_for_pooled_timings(cur_cluster_system,cluster_runs_info)
+        self.ranked_results_exist = check_for_ranked_results(cur_cluster_system,cluster_runs_info)
+        
+def check_for_any_scripts(self, cur_cluster_system, cluster_runs_info):
+    pass
+        
+def check_for_all_scripts(self, cur_cluster_system, cluster_runs_info):
+    pass
+
+def check_for_runs_in_progress(self, cur_cluster_system, cluster_runs_info):
+    pass
+
+def check_for_runs_complete(self, cur_cluster_system, cluster_runs_info):
+    pass
+
+def check_for_pooled_results(self, cur_cluster_system, cluster_runs_info):
+    pass
+
+def check_for_pooled_timings(self, cur_cluster_system, cluster_runs_info):
+    pass
+
+def check_for_ranked_results(self, cur_cluster_system, cluster_runs_info):
+    pass
